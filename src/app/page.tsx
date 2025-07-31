596bb61358a2ce8fe5bcccefe8da6de23d59e26a
francescovirgiliopopescu <francescovirgiliopopescu@gmail.com>
2025-07-31 21:02:00 +1000

    feat: basic weather forecast app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,184 @@
-import Image from "next/image";
+"use client";
+
+import {
+  CitySearchType,
+  CurrentWeatherType,
+  DailytWeatherType,
+  HourlyWeatherType,
+  getWeather,
+} from "@/api/APICalls";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { useCallback, useEffect, useState } from "react";
+import DayCard from "../components/card/DayCard";
+import Header from "../components/header/Header";
+import TableRow from "../components/table/TableRow";
+import Search from "@/components/search/Search";
+const HOURLY_DATA_DISPLAY_LIMIT: number = 10;
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [currentData, setCurrentData] = useState<CurrentWeatherType>();
+  const [dailyData, setDailyData] = useState<DailytWeatherType[]>([]);
+  const [hourlyData, setHourlyData] = useState<HourlyWeatherType[]>([]);
+  const [selectedResult, setSelectedResult] = useState<CitySearchType>(
+    {} as CitySearchType
+  );
+  const [currentHourlyDispayIndex, setCurrentHourlyDispayIndex] =
+    useState<number>(0);
+  const [hourlyDisplayData, setHourlyDisplayData] = useState<
+    HourlyWeatherType[]
+  >([]);
+  const [location, setLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  }>({ latitude: 10, longitude: 10 });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        ({ coords }) => {
+          const { latitude, longitude } = coords;
+          setLocation({ latitude, longitude });
+        },
+        (error) => {
+          alert(
+            "There was an error getting your location. Please allow to use your location and refresh the page."
+          );
+        }
+      );
+    }
+  }, []);
+
+  const limitHourlyData = useCallback(
+    (data: HourlyWeatherType[]) => {
+      const newData = data.slice(
+        0,
+        HOURLY_DATA_DISPLAY_LIMIT + currentHourlyDispayIndex
+      );
+      // setHourlyDisplayData(prevData => [...prevData, ...newData]);
+      setHourlyDisplayData(newData);
+    },
+    [currentHourlyDispayIndex]
+  );
+
+  useEffect(() => {
+    const getData = async () => {
+      const latitude =
+        Object.keys(selectedResult).length > 0
+          ? selectedResult.latitude
+          : location.latitude;
+      const longitude =
+        Object.keys(selectedResult).length > 0
+          ? selectedResult.longitude
+          : location.longitude;
+
+      const weatherData = await getWeather(
+        latitude,
+        longitude,
+        Intl.DateTimeFormat().resolvedOptions().timeZone
+      );
+
+      if (weatherData.current) {
+        setCurrentData(weatherData.current);
+      }
+
+      if (weatherData.daily) {
+        setDailyData(weatherData.daily);
+      }
+
+      if (weatherData.hourly) {
+        limitHourlyData(weatherData.hourly);
+        setHourlyData(weatherData.hourly);
+      }
+    };
+
+    getData();
+  }, [location, limitHourlyData, selectedResult]);
+
+  function onClickResultHandler(item: CitySearchType): void {
+    setSelectedResult(item);
+  }
+
+  // useEffect(() => {
+  //   limitHourlyData(hourlyData);
+  // }, [hourlyData, limitHourlyData]);
+
+  // if(currentData == null || currentData == undefined){
+  //   return '';
+  // }
+
+  return (
+    // className="flex min-h-screen flex-col items-center justify-between p-24"
+    <main className={`${currentData == null ? "blur-md" : ""} `}>
+      {Object.keys(selectedResult).length > 0 && (
+        <div className="flex justify-center items-center mt-6">
+          <FaMapMarkerAlt className="w-6 h-6 object-contain" />
+          <div className="text-center pl-2 text-foregroundColor text-2xl font-medium ">
+            {selectedResult.name}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+      )}
+      <Search
+        latitude={location.latitude}
+        longitude={location.longitude}
+        onClickResultHandler={onClickResultHandler}
+      />
+      <Header
+        currentTemp={currentData?.currentTemp}
+        highTemp={currentData?.highTemp}
+        lowTemp={currentData?.lowTemp}
+        highFeelsLike={currentData?.highFeelsLike}
+        lowFeelsLike={currentData?.lowFeelsLike}
+        windSpeed={currentData?.windSpeed}
+        precip={currentData?.precip}
+        iconCode={currentData?.iconCode}
+      />
+      <section className="grid grid-cols-[repeat(auto-fit,160px)] gap-2 justify-center p-4">
+        {dailyData.map((item, index) => (
+          <DayCard
+            key={index}
+            iconCode={item.iconCode}
+            timestamp={item.timestamp}
+            degree={item.maxTemp}
+            // className="border-red-600"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        ))}
+        {/* <DayCard
+          // icon={<FaSun className="w-16 h-16" />}
+          iconCode={999}
+          day="Monday"
+          degree={32}
+        /> */}
+      </section>
+
+      <table className="w-full max-w-[1160px] m-auto mt-10 text-center border-spacing-0">
+        <tbody>
+          {hourlyDisplayData.map((item, index) => (
+            <TableRow
+              key={index}
+              maxTemp={item.maxTemp}
+              feelsLike={item.feelsLike}
+              precip={item.precip}
+              timestamp={item.timestamp}
+              windSpeed={item.windSpeed}
+              iconCode={item.iconCode}
+            />
+          ))}
+        </tbody>
+      </table>
+      {hourlyDisplayData.length < hourlyData.length && (
+        <div className="flex justify-center items-center my-5">
+          <button
+            onClick={() =>
+              setCurrentHourlyDispayIndex(
+                currentHourlyDispayIndex + HOURLY_DATA_DISPLAY_LIMIT
+              )
+            }
+            className="p-4 m-4 border rounded-lg border-foregroundColor bg-foregroundColor text-white text-sm"
+          >
+            Load More
+          </button>
+        </div>
+      )}
+    </main>
   );
 }
